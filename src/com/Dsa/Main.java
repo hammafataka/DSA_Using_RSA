3a5236ccc5f1912be6fbeafc19a2b9f91efc71bd
hammafataka <66084806+hammafataka@users.noreply.github.com>
2021-04-28 19:52:44 +0200

    initail commit

--- src/com/Dsa/Main.java
+++ src/com/Dsa/Main.java
@@ -79,53 +79,6 @@ public class  Main extends JFrame {
         FileDetails.add(filePath);
         FileDetails.add(importBtn);
 
-        importBtn.addActionListener(e -> {
-                    try {
-                        JFileChooser file = new JFileChooser();
-                        file.setCurrentDirectory(new File(System.getProperty("user.home")));
-                        FileNameExtensionFilter filter = new FileNameExtensionFilter("DSA", ".txt");
-                        file.addChoosableFileFilter(filter);
-                        int result = file.showSaveDialog(null);
-                        if (result == JFileChooser.APPROVE_OPTION) {
-                            File selectedFile = file.getSelectedFile();
-                            absolutePath = selectedFile.getAbsolutePath();
-                            pathFile = new File(absolutePath);
-                            filePath.setText(absolutePath);
-                            String fileName = pathFile.getName();
-
-                            toReturn = readFileAsString(absolutePath);
-                            Name.setText("Name: "+fileName);
-                            TypeOfFile.setText("Type Of File: .txt");
-                            Location.setText("Location: "+absolutePath);
-                            double dsize=(double) pathFile.length()   ;
-                            DecimalFormat df=new DecimalFormat();
-                            df.setMaximumFractionDigits(2);
-                            try{
-                               Path p=pathFile.toPath();
-                                FileTime createdtime=(FileTime) Files.getAttribute(p,"creationTime");
-
-                                Created.setText("Created: "+createdtime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
-                            }
-                            catch (IOException ignored) {
-
-                            }
-                            String filesize=df.format(dsize)+" Bytes";
-                            size.setText("Size: "+filesize);
-                            Hashed=hashing(toReturn);
-                            DateFormat sdf=new SimpleDateFormat("MMMM dd, yyyy hh:mm a");
-                            Modified.setText("Modified: "+sdf.format(pathFile.lastModified()));
-                           JOptionPane.showMessageDialog(null, Hashed);
-
-                        } else if (result == JFileChooser.CANCEL_OPTION) {
-                            JOptionPane.showMessageDialog(null, "No File Selected", "Failed", JOptionPane.ERROR_MESSAGE);
-                        }
-                    } catch (Exception exception) {
-                        log.info(exception.getMessage());
-                    }
-
-
-                });
-
         JPanel FileDescriptions = new JPanel();
         FileDescriptions.setLayout(new GridLayout(7, 2, 10, 10));
         FileDescriptions.setBorder(FileDescriptionBorder);
@@ -149,29 +102,6 @@ public class  Main extends JFrame {
 
         GenerateKeys = new JLabel("Generate a Public/Private Key Pair");
         Generate = new JButton("Generate");
-        Generate.addActionListener(e -> {
-            publictemp=GenerateRandom();
-            privatetemp=GenerateRandom();
-            while (publictemp==privatetemp||publictemp<privatetemp){
-
-                privatetemp=GenerateRandom();
-
-
-            }
-
-            File tempfile=selectfile();
-            GenerateFile(tempfile,publictemp);
-
-
-            RSA r=new RSA();
-            r.labelP.setText(Integer.toString(publictemp));
-            r.labelQ.setText(Integer.toString(privatetemp));
-            r.setSize(800, 350);
-            r.setVisible(rootPaneCheckingEnabled);
-            r.enterMessage.setText(Hashed);
-
-
-        });
         PublicKey = new JLabel("Public Key");
         PrivateKey = new JLabel("Private Key");
         pField = new JTextField();
@@ -198,42 +128,111 @@ public class  Main extends JFrame {
         add(FileDescriptions, BorderLayout.CENTER);
         add(KeyDetails, BorderLayout.SOUTH);
 
+        Generate.addActionListener(e -> {
+            publictemp=GenerateRandom();
+            privatetemp=GenerateRandom();
+            while (publictemp==privatetemp||publictemp<privatetemp){
+                privatetemp=GenerateRandom();
+            }
+            CreateFile(publictemp,"public","txt");
+            RSA r=new RSA();
+            r.labelP.setText(Integer.toString(publictemp));
+            r.labelQ.setText(Integer.toString(privatetemp));
+            r.setSize(800, 350);
+            r.setVisible(rootPaneCheckingEnabled);
+            r.enterMessage.setText(Hashed);
 
+        });
+
+        importBtn.addActionListener(e -> {
+            try {
+                JFileChooser file = new JFileChooser();
+                file.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = file.showSaveDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = file.getSelectedFile();
+                    absolutePath = selectedFile.getAbsolutePath();
+                    pathFile = new File(absolutePath);
+                    filePath.setText(absolutePath);
+                    String fileName = pathFile.getName();
+
+                    toReturn = readFileAsString(absolutePath);
+                    Name.setText("Name: "+fileName);
+                    TypeOfFile.setText("Type Of File: .txt");
+                    Location.setText("Location: "+absolutePath);
+                    double dsize=(double) pathFile.length()   ;
+                    DecimalFormat df=new DecimalFormat();
+                    df.setMaximumFractionDigits(2);
+                    try{
+                        Path p=pathFile.toPath();
+                        FileTime createdtime=(FileTime) Files.getAttribute(p,"creationTime");
+
+                        Created.setText("Created: "+createdtime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+                    }
+                    catch (IOException ignored) {
+
+                    }
+                    String filesize=df.format(dsize)+" Bytes";
+                    size.setText("Size: "+filesize);
+                    Hashed=hashing(toReturn);
+                    DateFormat sdf=new SimpleDateFormat("MMMM dd, yyyy hh:mm a");
+                    Modified.setText("Modified: "+sdf.format(pathFile.lastModified()));
+                    JOptionPane.showMessageDialog(null, Hashed);
+
+                } else if (result == JFileChooser.CANCEL_OPTION) {
+                    JOptionPane.showMessageDialog(null, "No File Selected", "Failed", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception exception) {
+                log.info(exception.getMessage());
+            }
+
+
+        });
     }
-    private static void GenerateFile(File file,int toWrite){
 
-        try{
-            BufferedWriter bd=new BufferedWriter(new FileWriter(file));
-            bd.write(String.format("%d",toWrite));
-            bd.close();
-        }catch (Exception ex){
-            JOptionPane.showMessageDialog(null,ex.getMessage());
+
+    private static File GetSelectedFile(){
+        JFileChooser file=new JFileChooser();
+        file.setCurrentDirectory(new File(System.getProperty("*")));
+        int result=file.showSaveDialog(null);
+        File selectedFile=new File("");
+        if(result==JFileChooser.APPROVE_OPTION){
+            selectedFile=file.getSelectedFile();
+
         }
+        else if (result == JFileChooser.CANCEL_OPTION){
+            JOptionPane.showMessageDialog(null, "No File Selected", "Failed", JOptionPane.ERROR_MESSAGE);
+        }
+        return selectedFile;
 
     }
-    private static File  selectfile(){
+
+    private static void CreateFile(int toWrite,String filename,String extension){
         JFileChooser file = new JFileChooser();
         file.setCurrentDirectory(new File(System.getProperty("user.home")));
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("DSA", ".txt");
-        file.addChoosableFileFilter(filter);
         file.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        int result = 0;//file.showSaveDialog(null);
         File tempfile=new File("");
         if (file.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             File selectedFile=file.getSelectedFile();
             String stringpath=selectedFile.getAbsolutePath();
             Path path=Paths.get(stringpath);
-             tempfile=new File(String.format("%s//public.txt",path));
+             tempfile=new File(String.format("%s//%s.%s",path,filename,extension));
+            try{
+                BufferedWriter bd=new BufferedWriter(new FileWriter(tempfile));
+                bd.write(String.format("%d",toWrite));
+                bd.close();
+            }
+            catch (Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage());
+            }
 
-
-
-
-
-        } else if (result == JFileChooser.CANCEL_OPTION) {
-            JOptionPane.showMessageDialog(null, "No File Selected", "Failed", JOptionPane.ERROR_MESSAGE);
         }
-        return tempfile;
+        else{
+            JOptionPane.showMessageDialog(null, "No Directory Selected", "Failed", JOptionPane.ERROR_MESSAGE);
+        }
     }
+
+
     private static int GenerateRandom(){
         Random rand=new Random();
         int num=rand.nextInt(400);
